46d7662815c130414ef6a41954762c6f0872bd7c
Migiro-Johans <migironyogora@gmail.com>
2025-09-11 19:24:34 -0700

    The counter feature

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
   return (
     <>
       <div>
+	  <Counter/>
 	  <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
 	  <Header />
 	  <MainContent />
