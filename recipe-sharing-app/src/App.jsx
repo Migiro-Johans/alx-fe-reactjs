1218781210bfca8e5e49bc4f706a9dfeff20af7f
Migiro-Johans <migironyogora@gmail.com>
2025-09-21 03:18:38 -0700

    Recipe_app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RecipeList from './components/RecipeList.jsx'
+import AddRecipeForm from './components/AddRecipeForm.jsx'
+function App() {
+  const [Recipe, newRecipe] = useState();
+
+  return (
+    <>
+	  <div>
+	  	<RecipeList/>
+	  </div>
+	  <div>
+	 	<AddRecipeForm/>
+	  </div>
+    </>
+  )
+}
+
+export default App
