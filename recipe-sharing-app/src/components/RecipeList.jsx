3b859f7ee2df945e002830a698017439f04a8389
Migiro-Johans <migironyogora@gmail.com>
2025-09-21 05:26:07 -0700

    recipeList

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useRecipeStore } from "./recipeStore";
+import FavoriteButton from "./FavoriteButton";
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore((s) => s.filteredRecipes);
@@ -12,6 +13,7 @@ const RecipeList = () => {
         <div key={r.id} style={{ marginBottom: "1rem" }}>
           <h3 style={{ marginBottom: ".25rem" }}>
             <Link to={`/recipes/${r.id}`}>{r.title}</Link>
+            <FavoriteButton recipeId={r.id} />
           </h3>
           <p style={{ margin: 0 }}>{r.description}</p>
           <div style={{ marginTop: ".25rem" }}>
